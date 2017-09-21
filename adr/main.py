9a7f203d6a9aad28e5cd751b07d1be90cce2e576
Andrew Halberstadt <halbersa@gmail.com>
2017-09-21 16:03:55 -0400

    Add missing __init__.py

--- adr/main.py
+++ adr/main.py
@@ -8,13 +8,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 RECIPE_DIR = os.path.join(here, 'recipes')
 
 
-def run_recipe(name, args):
-
-
 class RecipeParser(ArgumentParser):
 
     def __init__(self, *args, **kwargs):
-        ArgumentParser.__init__(*args, **kwargs)
+        ArgumentParser.__init__(self, *args, **kwargs)
 
         self.add_argument('recipe', nargs='?', help="Recipe to run.")
         self.add_argument('-l', '--list', action='store_true', default=False,
@@ -24,10 +21,6 @@ class RecipeParser(ArgumentParser):
 def cli(args=sys.argv[1:]):
     parser = RecipeParser()
     args, remainder = parser.parse_known_args(args)
-
-    if args.list:
-        recipes = os.listdir(RECIPE_DIR)
-        return
 
     for path in sorted(os.listdir(RECIPE_DIR)):
         name = os.path.splitext(path)[0]
