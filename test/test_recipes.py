016a1d2fb1fc5a454ca7bdbacc77a39c9369c3e9
Andrew Halberstadt <halbersa@gmail.com>
2017-11-17 16:52:14 -0500

    Reload recipe modules between tests if necessary

--- test/test_recipes.py
+++ test/test_recipes.py
@@ -2,6 +2,9 @@ from __future__ import print_function, absolute_import
 
 import json
 import os
+import sys
+from imp import reload
+from io import StringIO
 
 import pytest
 import yaml
@@ -9,34 +12,32 @@ import yaml
 from adr import query
 from adr.main import run_recipe
 
-here = os.path.abspath(os.path.dirname(__file__))
 
-RECIPES = {
-    'backout_rate': [],
-    'code_coverage': ['--rev', '123456789abc', '--path', 'foo'],
-    'code_coverage_by_suite': ['--rev', '123456789abc', '--path', 'foo'],
-    'files_with_coverage': [],
-    'hours_on_try': [],
-    'inspect': [],
-    'raw_coverage': ['--rev', '123456789abc', '--path', 'foo'],
-    'task_durations': [],
-    'try_usage': [],
-    'try_users': [],
-}
+class new_run_query(object):
+    def __init__(self, test):
+        self.test = test
+
+    def __call__(self, query, *args, **kwargs):
+        print(self.test['queries'].keys())
+        if query not in self.test['queries']:
+            pytest.fail("no test data found for query '{}' in '{}.test'".format(
+                        query, self.test['recipe']))
+        for result in self.test['queries'][query]:
+            yield result
 
 
 def test_recipe(monkeypatch, recipe_test):
+    monkeypatch.setattr(query, 'run_query', new_run_query(recipe_test))
 
-    def _run_query(query, *args, **kwargs):
-        if query not in recipe_test['queries']:
-            pytest.fail("no test data found for query '{}' in '{}.test'".format(
-                        query, recipe_test['recipe']))
-        for result in recipe_test['queries'][query]:
-            yield result
+    module = 'adr.recipes.{}'.format(recipe_test['recipe'])
+    if module in sys.modules:
+        reload(sys.modules[module])
 
-    monkeypatch.setattr(query, 'run_query', _run_query)
 
     result = json.loads(run_recipe(recipe_test['recipe'], recipe_test['args'], fmt='json'))
-    print("Copy/paste result:")
-    print(result)
+
+    buf = StringIO()
+    yaml.dump(result, buf)
+    print("Yaml formatted result for copy/paste:")
+    print(buf.getvalue())
     assert result == recipe_test['expected']
