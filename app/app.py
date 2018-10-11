fdb8ef6469bebc9fcb879797a83413c4798640f2
nadaa <nada.saif7@gmail.com>
2018-10-11 23:09:08 +0300

    made changes on app

--- app/app.py
+++ app/app.py
@@ -1,19 +1,20 @@
 from flask import Flask,render_template,Markup
 import os
-from adr.recipe import run_recipe
 import markdown
+from adr import recipes
+from adr.recipe import run_recipe
 
 app=Flask(__name__)
-recipes=[]
+recipe_names=[]
+recipe_path = os.path.dirname(recipes.__file__)
 
 def get_recipes():
     """
     Return a list of recipes located in /adr/recipes path
     """
-    for root, dirs, files in os.walk("../adr/recipes"):
-        for filename in files:
-            if filename.endswith('.py') and  filename!='__init__.py':
-                recipes.append(filename[0:len(filename)-3])
+    for file in os.listdir(recipe_path):
+            if file.endswith('.py') and  file!='__init__.py':
+                recipe_names.append(file.rsplit('.',1)[0])
 get_recipes()
 
 @app.route('/')
@@ -22,7 +23,7 @@ def home():
     Render a template with the list of availble recipes
     :returns: template
     """
-    return render_template('home.html',recipes=recipes)
+    return render_template('home.html',recipes=recipe_names)
 
 @app.route('/runrecipe/<recipename>')
 def runrecipe(recipename):
@@ -32,7 +33,7 @@ def runrecipe(recipename):
     """
     result_md=run_recipe(recipename, None,'markdown')
     table_html=markdown.markdown(result_md, extensions=['markdown.extensions.tables'])
-    return render_template('home.html',recipes=recipes,format=Markup(table_html))
+    return render_template('home.html',recipes=recipe_names,format=Markup(table_html))
 
 if __name__=='__main__':
     app.run(debug=True)
