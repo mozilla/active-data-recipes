3d8b251868a03643104870dd2365b2cf5168e76a
nikkis <nikkisharpley@gmail.com>
2018-10-17 16:19:38 -0400

    Added monkeypatch to test_queries.py For the test_query() function in test_queries.py, replaced 'query_activedata' attribute from query  module with RunQuery object that, when called, returns mocked data from the query_tests files.

--- test/test_queries.py
+++ test/test_queries.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, absolute_import
+from __future__ import print_function, absolute_import, unicode_literals
 
 import json
 import sys
@@ -9,7 +9,8 @@ import pytest
 import yaml
 
 from adr import formatter
-from adr.query import format_query, run_query, query_activedata
+from adr import query
+from adr.query import format_query, query_activedata
 from adr.util.config import Configuration
 
 if sys.version_info > (3, 0):
@@ -19,23 +20,28 @@ else:
 
 @pytest.fixture
 def config():
-    congif = Configuration()
+    config = Configuration()
     config.debug = False
     config.fmt = 'json'
     config.url = "http://activedata.allizom.org/query"
     return config
 
-def test_query(query_test, config):
+class RunQuery(object):
+    def __init__(self, query_test):
+        self.query_test = query_test
+
+    def __call__(self, query, *args, **kwargs):
+        return self.query_test['mock_data']
+
+def test_query(monkeypatch, query_test, config):
+
+    monkeypatch.setattr(query, 'query_activedata', RunQuery(query_test))
     module = 'adr.queries.{}'.format(query_test['query'])
     if module in sys.modules:
         reload(sys.modules[module])
 
-    result = json.loads(format_query(query_test['query'], config))
-    #Line 33 - having trouble converting format to json as in terminal the 
-    #following error appears:
-    #TypeError: expected string or buffer
-    #It could be because config is an object? Not sure how to resolve...
-    
+    result = json.loads(format_query(query_test['query'], config)[0])
+   
     buf = IO()
     yaml.dump(result, buf)
     print("Yaml formatted result for copy/paste:")
@@ -45,4 +51,5 @@ def test_query(query_test, config):
     yaml.dump(query_test['expected'], buf)
     print("\nYaml formatted expected:")
     print(buf.getvalue())
-    assert result == query_test['expected']
+    assert result == query_test["expected"]
+
