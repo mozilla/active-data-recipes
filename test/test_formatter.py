e2687ba438797791b57837979a75680018718836
MadinaB <madinabektayeva@gmail.com>
2018-11-06 18:44:21 +0900

    moved to pytest

--- test/test_formatter.py
+++ test/test_formatter.py
@@ -1,142 +1,59 @@
 from adr.formatter import JSONFormatter, TabFormatter, TableFormatter
 from terminaltables import GithubFlavoredMarkdownTable, SingleTable
 import json
-import unittest
+import pytest
+import yaml
 
 
-class TestJSONFormatter(unittest.TestCase):
-    actual = [('Table',), ('coverage',), ('firefox-files',), ('fx-test',), ('repo',),
-              ('saved_queries',), ('task',), ('treeherder',), ('unittest',)]
-
-    expected = """[
-  [
-    "Table"
-  ],
-  [
-    "coverage"
-  ],
-  [
-    "firefox-files"
-  ],
-  [
-    "fx-test"
-  ],
-  [
-    "repo"
-  ],
-  [
-    "saved_queries"
-  ],
-  [
-    "task"
-  ],
-  [
-    "treeherder"
-  ],
-  [
-    "unittest"
-  ]
-]"""
+class TestJSONFormatter(object):
 
     def test_init_none(self):
         f = JSONFormatter()
-        self.assertEqual(f.indent, None)
+        assert f.indent is None
 
     def test_init(self):
         f = JSONFormatter(4)
-        self.assertEqual(f.indent, 4)
+        assert f.indent == 4
 
-    def test_call(self):
+    def test_call_bytes(self, json_formatter_test):
         f = JSONFormatter(2)
-        self.assertEqual(f(self.actual), self.expected)
+        actual = f(json_formatter_test['expected'].encode())
+        expected = json_formatter_test['expected']
+        assert actual == expected
 
-    def test_call_bytes(self):
+    def test_call(self, json_formatter_test):
         f = JSONFormatter(2)
-        self.assertEqual(f(self.expected.encode()), self.expected)
+        actual = f(json_formatter_test['actual'])
+        expected = json_formatter_test['expected']
+        assert actual == expected
 
 
-class TestTabFormatter(unittest.TestCase):
-    actual = [('Table',), ('activedata_requests',), ('coverage',), ('firefox-files',),
-              ('fx-test',), ('jobs',), ('repo',), ('saved_queries',), ('task',),
-              ('treeherder',), ('unittest',)]
-    expected = """Table
-activedata_requests
-coverage
-firefox-files
-fx-test
-jobs
-repo
-saved_queries
-task
-treeherder
-unittest"""
+class TestTabFormatter():
 
-    def test_non_list(self):
+    def test_call(self, tab_formatter_test):
         f = TabFormatter()
-        data = {'key': 'value'}
-        with self.assertRaises(Exception) as expected:
-            f(data)
-        self.assertTrue('expecting a list' in str(expected.exception))
+        actual = f(tab_formatter_test['actual'])
+        expected = tab_formatter_test['expected']
+        assert actual == expected
 
-    def test_call(self):
+    def test_call_bytes(self, tab_formatter_test):
         f = TabFormatter()
-        self.assertEqual(f(self.actual), self.expected)
-
-    def test_call_bytes(self):
-        f = TabFormatter()
-        actual_bytes = json.dumps(self.actual).encode()
-        self.assertEqual(f(actual_bytes), self.expected)
+        actual = f(json.dumps(tab_formatter_test['actual']).encode())
+        expected = tab_formatter_test['expected']
+        assert actual == expected
 
 
-class TestTableFormatter(unittest.TestCase):
+class TestTableFormatter():
     def test_init(self):
         f = TableFormatter()
-        self.assertEqual(f.table_cls, SingleTable)
+        assert f.table_cls == SingleTable
 
     def test_init_table(self):
         f = TableFormatter(GithubFlavoredMarkdownTable)
-        self.assertEqual(f.table_cls, GithubFlavoredMarkdownTable)
+        assert f.table_cls == GithubFlavoredMarkdownTable
 
-    def test_call_markdown(self):
-        actual = [('Table',), ('activedata_requests',), ('coverage',), ('firefox-files',),
-                  ('fx-test',),
-                  ('jobs',), ('repo',), ('saved_queries',), ('task',), ('treeherder',),
-                  ('unittest',)]
-        expected = """| Table               |
-|---------------------|
-| activedata_requests |
-| coverage            |
-| firefox-files       |
-| fx-test             |
-| jobs                |
-| repo                |
-| saved_queries       |
-| task                |
-| treeherder          |
-| unittest            |"""
+    def test_call(self, table_formatter_test):
         f = TableFormatter(GithubFlavoredMarkdownTable)
-        self.assertEqual(f(actual), expected)
-
-    def test_call_dict(self):
-        f = TableFormatter(GithubFlavoredMarkdownTable)
-        actual = {'Table': ['activedata_requests', 'coverage', 'jobs', 'repo']}
-        expected = """| Table               |
-|---------------------|
-| activedata_requests |
-| coverage            |
-| jobs                |
-| repo                |"""
-        self.assertEqual(f(actual), expected)
-
-    def test_call_dict2(self):
-        f = TableFormatter(GithubFlavoredMarkdownTable)
-        actual = {'names': ['Table', 'Coverage'],
-                  'Table': ['activedata_requests', 'coverage', 'jobs', 'repo'],
-                  'Coverage': [0, 15, 55, 100]}
-        expected = """| Table               | Coverage |
-|---------------------|----------|
-| activedata_requests | 0        |
-| coverage            | 15       |
-| jobs                | 55       |
-| repo                | 100      |"""
-        self.assertEqual(f(actual), expected)
+        actual = f(table_formatter_test['actual'])
+        expected = table_formatter_test['expected']
+        assert actual == expected
