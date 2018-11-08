585d1233910fbaf6d4a3dbaadcece6c29b670f97
Madina <MadinaB@users.noreply.github.com>
2018-11-08 22:28:12 +0900

    test_formatter created (#76)
    * test_formatter created
    
    * autopep8 to avoid flake8
    
    * moved to pytest
    
    * correcting flake8 violations

--- test/conftest.py
+++ test/conftest.py
@@ -25,6 +25,16 @@ def load_query_tests():
                 yield test
 
 
+def load_formatter_tests(format):
+    formatter_dir = os.path.join(here, 'formatter_tests')
+    json_formatter_tests = [os.path.join(formatter_dir, j) for j in os.listdir(
+        formatter_dir) if j.endswith('.test') and j.startswith(format)]
+    for json_formatter_test in json_formatter_tests:
+        with open(json_formatter_test, 'r') as fh:
+            for test in yaml.load_all(fh):
+                yield test
+
+
 def recipe_idfn(val):
     return '{} {}'.format(val['recipe'], ' '.join(val['args'])).strip()
 
@@ -33,8 +43,30 @@ def query_idfn(val):
     return '{} {}'.format(val['query'], ' '.join(val['args'])).strip()
 
 
+def formatter_idfn(val):
+    return '{} {}'.format(val['format'], ' '.join(val['args'])).strip()
+
+
 def pytest_generate_tests(metafunc):
     if 'recipe_test' in metafunc.fixturenames:
         metafunc.parametrize('recipe_test', list(load_recipe_tests()), ids=recipe_idfn)
     if 'query_test' in metafunc.fixturenames:
         metafunc.parametrize('query_test', list(load_query_tests()), ids=query_idfn)
+    if 'json_formatter_test' in metafunc.fixturenames:
+        metafunc.parametrize(
+            'json_formatter_test',
+            list(
+                load_formatter_tests('json_')),
+            ids=formatter_idfn)
+    if 'tab_formatter_test' in metafunc.fixturenames:
+        metafunc.parametrize(
+            'tab_formatter_test',
+            list(
+                load_formatter_tests('tab_')),
+            ids=formatter_idfn)
+    if 'table_formatter_test' in metafunc.fixturenames:
+        metafunc.parametrize(
+            'table_formatter_test',
+            list(
+                load_formatter_tests('table_')),
+            ids=formatter_idfn)
---
+++ test/test_formatter.py
@@ -0,0 +1,57 @@
+from adr.formatter import JSONFormatter, TabFormatter, TableFormatter
+from terminaltables import GithubFlavoredMarkdownTable, SingleTable
+import json
+
+
+class TestJSONFormatter(object):
+
+    def test_init_none(self):
+        f = JSONFormatter()
+        assert f.indent is None
+
+    def test_init(self):
+        f = JSONFormatter(4)
+        assert f.indent == 4
+
+    def test_call_bytes(self, json_formatter_test):
+        f = JSONFormatter(2)
+        actual = f(json_formatter_test['expected'].encode())
+        expected = json_formatter_test['expected']
+        assert actual == expected
+
+    def test_call(self, json_formatter_test):
+        f = JSONFormatter(2)
+        actual = f(json_formatter_test['actual'])
+        expected = json_formatter_test['expected']
+        assert actual == expected
+
+
+class TestTabFormatter(object):
+
+    def test_call(self, tab_formatter_test):
+        f = TabFormatter()
+        actual = f(tab_formatter_test['actual'])
+        expected = tab_formatter_test['expected']
+        assert actual == expected
+
+    def test_call_bytes(self, tab_formatter_test):
+        f = TabFormatter()
+        actual = f(json.dumps(tab_formatter_test['actual']).encode())
+        expected = tab_formatter_test['expected']
+        assert actual == expected
+
+
+class TestTableFormatter(object):
+    def test_init(self):
+        f = TableFormatter()
+        assert f.table_cls == SingleTable
+
+    def test_init_table(self):
+        f = TableFormatter(GithubFlavoredMarkdownTable)
+        assert f.table_cls == GithubFlavoredMarkdownTable
+
+    def test_call(self, table_formatter_test):
+        f = TableFormatter(GithubFlavoredMarkdownTable)
+        actual = f(table_formatter_test['actual'])
+        expected = table_formatter_test['expected']
+        assert actual == expected
