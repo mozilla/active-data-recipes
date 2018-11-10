edc389bdb14a7bed30651763684ff02ea7b00778
MadinaB <madinabektayeva@gmail.com>
2018-11-10 23:01:35 +0900

    Optional filters

--- adr/recipes/skipped_tests.py
+++ adr/recipes/skipped_tests.py
@@ -9,38 +9,6 @@ from __future__ import print_function, absolute_import
 from ..query import run_query
 from argparse import ArgumentParser
 
-all_suites = ["mochitest-browser-chrome-instrumentation",
-              "web-platform-tests-reftests",
-              "mochitest-devtools-chrome",
-              "mochitest-browser-chrome",
-              "mochitest-valgrind-plain",
-              "mochitest-webgl1-core",
-              "mochitest-webgl2-core",
-              "mochitest-webgl1-ext",
-              "mochitest-webgl2-ext",
-              "mochitest-clipboard",
-              "web-platform-tests",
-              "reftest-jsreftest",
-              "reftest-crashtest",
-              "reftest-no-accel",
-              "mochitest-chrome",
-              "mochitest-media",
-              "mochitest-plain",
-              "mochitest-a11y",
-              "mochitest-gpu",
-              "reftest-gpu",
-              "test-verify",
-              "marionette",
-              "xpcshell",
-              "reftest"]
-
-all_platforms = ["linux64",
-                 "linux32",
-                 "maxosx64",
-                 "android",
-                 "win64",
-                 "win32"]
-
 
 def run(args, config):
     parser = ArgumentParser()
@@ -51,14 +19,18 @@ def run(args, config):
                         help="Ending date to pull data from, defaults to end of the day")
     parser.add_argument('--limit', type=int, default=100, dest='limit',
                         help="Maximum number of tests to return")
-    parser.add_argument('--suite', default=all_suites, dest='suite',
+    parser.add_argument('--suite', dest='suite',
                         help="Select suite to get skipped tests from." +
                         "By default, gets data from all suites.")
-    parser.add_argument('--platform', default=all_platforms, dest='platform',
+    parser.add_argument('--platform', dest='platform',
                         help="Select platform to get skipped tests from." +
                         "By default, gets information about tests across all platforms.")
     args = parser.parse_args(args)
     query_args = vars(args)
-    result = next(run_query('skipped_tests', config, **query_args))['data']
+    result = next(
+        run_query(
+            'skipped_tests',
+            config,
+            **query_args))['data']
     result.insert(0, ['Result test', 'run suite', 'count'])
     return result
