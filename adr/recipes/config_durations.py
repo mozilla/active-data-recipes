55f62f2c6c4cbab6a451ec029779e5a2adffc1b3
MadinaB <madinabektayeva@gmail.com>
2018-11-11 20:12:25 +0900

    added config_durations

--- adr/recipes/config_durations.py
+++ adr/recipes/config_durations.py
@@ -6,23 +6,29 @@ Get the average and total runtime for build platforms and types.
     adr config_durations [--branch <branch>]
 """
 from __future__ import print_function, absolute_import
-
-from ..recipe import RecipeParser
+from adr.util.modified_docopt import docopt
 from ..query import run_query
 
 
+argument_parser = """
+    Argument parser for the config_durations recipe.
+
+    Usage:
+    config_durations [--limit LIMIT] [--sort-key SORTKEY]
+
+    Options:
+    --limit=LIMIT  Maximum number of jobs to return. [default: 50]
+    --sort-key=SORTKEY  Key to sort on (int, 0-based index). [default: 4]
+    """
+
+
 def run(args, config):
-    parser = RecipeParser('date', 'branch')
-    parser.add_argument('--limit', type=int, default=50,
-                        help="Maximum number of jobs to return")
-    parser.add_argument('--sort-key', type=int, default=4,
-                        help="Key to sort on (int, 0-based index)")
 
-    args = parser.parse_args(args)
-    query_args = vars(args)
-    limit = query_args.pop('limit')
+    limit = int(docopt(argument_parser, args)['--limit'])
+    sort_key = int(docopt(argument_parser, args)['--sort-key'])
 
-    data = next(run_query('config_durations', config, **query_args))['data']
+    data = next(run_query('config_durations', config, *args))['data']
+
     result = []
     for record in data:
         if isinstance(record[1], list):
@@ -35,6 +41,6 @@ def run(args, config):
         record.append(int(round(record[2] * record[3], 0)))
         result.append(record)
 
-    result = sorted(result, key=lambda k: k[args.sort_key], reverse=True)[:limit]
+    result = sorted(result, key=lambda k: k[sort_key], reverse=True)[:limit]
     result.insert(0, ['Platform', 'Type', 'Num Jobs', 'Average Hours', 'Total Hours'])
     return result
