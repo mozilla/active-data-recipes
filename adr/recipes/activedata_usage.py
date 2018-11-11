5833e3bcba0199441e18aa6bf0b237bf19d87f41
MadinaB <madinabektayeva@gmail.com>
2018-11-11 12:19:17 +0900

    active_data reordered for docopt

--- adr/recipes/activedata_usage.py
+++ adr/recipes/activedata_usage.py
@@ -1,10 +1,10 @@
 """
-Show ActiveData query usage, by day
-
-.. code-block:: bash
-
+    Show ActiveData query usage, by day
+    
+    .. code-block:: bash
+    
     adr activedata_usage [--from <date> [--to <date>]]
-"""
+    """
 
 from __future__ import print_function, absolute_import
 
@@ -13,9 +13,5 @@ from ..query import run_query
 
 
 def run(args, config):
-    parser = RecipeParser('date')
-    args = parser.parse_args(args)
-
-    query_args = vars(args)
-    response = next(run_query('activedata_usage', config, **query_args))
+    response = next(run_query('activedata_usage', config, args))
     return [response['header']] + response['data']
