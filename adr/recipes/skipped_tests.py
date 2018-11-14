dc2cd4c32ec84e53c4cb21301c06d483b92df1dd
Madina <MadinaB@users.noreply.github.com>
2018-11-15 00:49:16 +0900

    Recipe to track skipped tests (#69)
    * example recipe
    
    * adding to
    
    * recipe to track skipped tests adjusted
    
    * setting branch.name, grouping by suite.fullname, adding support for selecting build.platform and suite.fullname
    
    * Updating recipe to track skipped tests
    
    Setting branch.name
    Grouping by suite.fullname
    Adding support for selecting build.platform and suite.fullname
    Fixing flake8 violations
    
    * setting --max-line-length=100
    
    * Optional filters
    
    * sort the result by path

---
+++ adr/recipes/skipped_tests.py
@@ -0,0 +1,34 @@
+"""
+Get skipped tests on the ActiveData schema. The above command returns the
+test suites and their count which were skipped/disabled between specified period. Run:
+.. code-block:: bash
+    adr skipped_tests
+"""
+from __future__ import print_function, absolute_import
+
+from ..query import run_query
+from argparse import ArgumentParser
+
+
+def run(args, config):
+    parser = ArgumentParser()
+    parser.add_argument('--from', default='today', dest='from_date',
+                        help="Starting date to pull data from,"
+                        + " defaults to the beginning of today in GMT.")
+    parser.add_argument('--to', default='eod', dest='to_date',
+                        help="Ending date to pull data from, defaults to end of the day")
+    parser.add_argument('--limit', type=int, default=100, dest='limit',
+                        help="Maximum number of tests to return")
+    parser.add_argument('--suite', dest='suite',
+                        help="Select suite to get skipped tests from." +
+                        "By default, gets data from all suites.")
+    parser.add_argument('--platform', dest='platform',
+                        help="Select platform to get skipped tests from." +
+                        "By default, gets information about tests across all platforms.")
+    args = parser.parse_args(args)
+    query_args = vars(args)
+    result = next(
+        run_query('skipped_tests', config, **query_args))['data']
+    result.sort(key=lambda x: x[0])
+    result.insert(0, ['Result test', 'run suite', 'count'])
+    return result
