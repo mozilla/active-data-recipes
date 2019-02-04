146147a475c930ba6127b5d61def37bf1da81190
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-02-04 17:57:47 -0500

    looks a bit nicer

--- adr/recipes/perf_tp6_compare.py
+++ adr/recipes/perf_tp6_compare.py
@@ -29,15 +29,25 @@ def run(config, args):
     result = run_query("perf_tp6_compare", config, args)
 
     tests, revisions = result['edges']
-    header = ["Subtest"] + [p['name'] for p in revisions['domain']['partitions']]
+    header = ["Subtest"] + [p['name'] for p in revisions['domain']['partitions']] + ["Change"]
+    test_names = [p['name'] for p in tests['domain']['partitions']]
+    values = result['data']['result.stats.median']
 
     data = list(
-        [n] + v
-        for n, v in zip(
-            [p['name'] for p in tests['domain']['partitions']],
-            result['data']['result.stats.median']
-        )
+        [n] + [round(v) if v is not None else None for v in row] + [change(*row)]
+        for n, row in zip(test_names, values)
     )
 
     data.insert(0, header)
     return data
+
+
+def change(r1, r2):
+    try:
+        value = round((1 - r2 / r1) * 100)
+        if value >= 0:
+            return " " + str(value) + '%'
+        else:
+            return str(value) + '%'
+    except Exception:
+        return ""
