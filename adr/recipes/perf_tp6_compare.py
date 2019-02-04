88248a517e32d82ec400a0b004442cd060f71d3f
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-02-04 15:46:10 -0500

    try to add context variables

--- adr/recipes/perf_tp6_compare.py
+++ adr/recipes/perf_tp6_compare.py
@@ -15,23 +15,19 @@ import logging
 
 from ..query import run_query
 
-log = logging.getLogger('adr')
-
+log = logging.getLogger("adr")
 
 RUN_CONTEXTS = [
-    'platform',
-    'branches',
+    {"rev1": [["--r1", "--rev1"], {"type": str, "help": "Revision to compare"}]},
+    {"rev2": [["--r2", "--rev2"], {"type": str, "help": "Revision to compare"}]},
+    {"subtest": [["-t", "--test"], {"type": str, "help": "the subtest name (or suffix)"}]},
 ]
 
 
 def run(args, config):
-
     # PULL DATA FOR REVISIONS
 
-    limit = args.limit
-    delattr(args, 'limit')
-
-    data = run_query('perf_tp6_compare', config, args)['data']
+    data = run_query("perf_tp6_compare", config, args)["data"]
     result = []
     for record in data:
         if record[2] is None:
@@ -41,7 +37,5 @@ def run(args, config):
         result.append(record)
 
     result = sorted(result, key=lambda k: k[args.sort_key], reverse=True)[:limit]
-    result.insert(0, ['Taskname', 'Num Jobs', 'Average Hours', 'Total Hours'])
+    result.insert(0, ["Taskname", "Num Jobs", "Average Hours", "Total Hours"])
     return result
-
-
