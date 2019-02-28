a9a249eff4be2121dc34319d8a473d93a255e4e8
TrangEmily <huyentrang1511@gmail.com>
2019-02-28 10:31:56 -0800

    add calendar picker to every recipe input form

--- app/static/recipe.js
+++ app/static/recipe.js
@@ -74,3 +74,12 @@ $(function(){
         runRecipe(url, form.serialize());
     });
 });
+
+
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'left'
+    }, function(start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
