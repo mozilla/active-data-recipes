5aac39683dd453208850707362d00018b59cd928
TrangEmily <huyentrang1511@gmail.com>
2019-02-28 10:49:27 -0800

    change calendar to single input with range

--- app/static/recipe.js
+++ app/static/recipe.js
@@ -77,9 +77,23 @@ $(function(){
 
 
 $(function() {
+
     $('input[name="daterange"]').daterangepicker({
-        opens: 'left'
+        "singleDatePicker": true,
+        "showDropdowns": true,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        "startDate": "02/21/2019",
+        "endDate": "02/27/2019"
     }, function(start, end, label) {
-        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
     });
 });
+
+
